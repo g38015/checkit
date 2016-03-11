daf9e8d35c8daadb54c16ee5b698d33205dc0bde
Peter Hitchcock <phitchcock@me.com>
2016-03-11 10:51:45 -0800

    installed administrate

--- CheckitRails/config/routes.rb
+++ CheckitRails/config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
+  root to: "checks#index"
+
+  namespace :admin do
+    resources :checks
+
+    root to: "checks#index"
+  end
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :checks
     end
   end
-
-  root to: "checks#index"
 
   resources :checks do
     collection { post :import }
