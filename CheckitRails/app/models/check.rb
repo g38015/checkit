5493b5740f6514827f1ee51767704cdfcc10b0c9
Peter Hitchcock <phitchcock@me.com>
2016-04-04 13:37:51 -0700

    updated check model

--- CheckitRails/app/models/check.rb
+++ CheckitRails/app/models/check.rb
@@ -10,36 +10,35 @@ class Check < ActiveRecord::Base
   )
 
   scope :search_query, lambda { |query|
-  # Searches the students table on the 'first_name' and 'last_name' columns.
-  # Matches using LIKE, automatically appends '%' to each term.
-  # LIKE is case INsensitive with MySQL, however it is case
-  # sensitive with PostGreSQL. To make it work in both worlds,
-  # we downcase everything.
-  return nil  if query.blank?
+    # Searches the students table on the 'first_name' and 'last_name' columns.
+    # Matches using LIKE, automatically appends '%' to each term.
+    # LIKE is case INsensitive with MySQL, however it is case
+    # sensitive with PostGreSQL. To make it work in both worlds,
+    # we downcase everything.
+    return nil  if query.blank?
 
-  # condition query, parse into individual keywords
-  terms = query.downcase.split(/\s+/)
+    # condition query, parse into individual keywords
+    terms = query.downcase.split(/\s+/)
 
-  # replace "*" with "%" for wildcard searches,
-  # append '%', remove duplicate '%'s
-  terms = terms.map { |e|
-    # "%#{e.gsub('*', '')}%"
-    (e.gsub('', '%') + '%').gsub(/%+/, '%')
-  }
-  # configure number of OR conditions for provision
-  # of interpolation arguments. Adjust this if you
-  # change the number of OR conditions.
-  num_or_conds = 1
+    # replace "*" with "%" for wildcard searches,
+    # append '%', remove duplicate '%'s
+    terms = terms.map { |e|
+      "%#{e.gsub('*', '')}%"
+    }
+    # configure number of OR conditions for provision
+    # of interpolation arguments. Adjust this if you
+    # change the number of OR conditions.
+    num_or_conds = 1
 
-  clause = terms.map { |term|
+    clause = terms.map { |term|
       "(LOWER(checks.name) LIKE ?)"
     }.join(' AND '),
     *terms.map { |e| [e] * num_or_conds }.flatten
 
-  #byebug
+    #byebug
 
-  where(clause)
-}
+    where(clause)
+  }
   scope :sorted_by, lambda { |sort_key|
     # Sorts students by sort_key
 
